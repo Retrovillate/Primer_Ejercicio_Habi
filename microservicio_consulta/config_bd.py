c3b68dbd9a11de0b9c155d7937c13a8a56d23f6d
Retrovillate <33127399+Retrovillate@users.noreply.github.com>
2025-08-30 16:33:14 -0500

    Create config_bd.py

---
+++ microservicio_consulta/config_bd.py
@@ -0,0 +1,11 @@
+"""
+Archivo de configuración de la base de datos.
+"""
+
+BD_CONFIG = {
+    "host": "18.221.137.98",
+    "port": 3309,
+    "user": "pruebas",
+    "password": "VGbt3Day5R",
+    "database": "habi_db"
+}
